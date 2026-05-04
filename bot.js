3fc8f7ea841b66fb300768a435632e28202b06b0
plumbry <bryonyleenewnham@gmail.com>
2026-05-05 00:27:15 +0100

    test: welcome module

--- bot.js
+++ bot.js
@@ -4,14 +4,19 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 console.log("STEP 2: DISCORD IMPORTED");
 
+// 👇 ADD THIS LINE
+require("./welcome-ping");
+
+console.log("STEP 3: WELCOME MODULE LOADED");
+
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once("ready", () => {
+client.once("clientReady", () => {
   console.log("✅ Logged in as", client.user.tag);
 });
 
 client.login(process.env.DISCORD_TOKEN)
-  .then(() => console.log("STEP 3: LOGIN SUCCESS"))
+  .then(() => console.log("STEP 4: LOGIN SUCCESS"))
   .catch(err => console.error("LOGIN ERROR:", err));
